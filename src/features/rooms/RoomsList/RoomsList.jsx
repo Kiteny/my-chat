69472069f8ea1e81c8ef9006b18a25a732e60406
Artyom <bitcoinfeks@gmail.com>
2020-11-09 03:13:13 +0500

    Добавил кнопку выхода из аккаунта

--- src/features/rooms/RoomsList/RoomsList.jsx
+++ src/features/rooms/RoomsList/RoomsList.jsx
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 import Loader from '../../../ui/Loader/Loader';
 
 import { roomsActions, roomsSelectors } from '../_roomsSlice_';
+import { userActions } from '../../user';
+
 import './RoomList.scss';
 
 const { fetchRooms } = roomsActions;
+const { logout } = userActions;
+
 const { selectRoomsEntities, selectRoomsStatus } = roomsSelectors;
 
 const RoomsList = () => {
@@ -19,6 +23,10 @@ const RoomsList = () => {
     dispatch(fetchRooms());
   }, []);
 
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   const renderedRooms = Object.keys(rooms).map((key) => (
     <li key={key} className="room-list__list-item">
       <Link to={`/rooms/${key}`}>
@@ -29,6 +37,11 @@ const RoomsList = () => {
 
   return (
     <div className="room-list app-border">
+      <div className="room-list__top-bar">
+        <button className="room-list__logout" type="button" onClick={handleLogout}>
+          <i className="fa fa-sign-out" aria-hidden="true" />
+        </button>
+      </div>
       <h2 className="room-list__title">Комнаты</h2>
       <ul className="room-list__list">
         {roomsStatus === 'pending' && <div className="room-list__loader"><Loader /></div>}
